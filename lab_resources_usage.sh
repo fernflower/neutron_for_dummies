d0d14b615c0b27c8c46611756827593447afbedb
Inessa Vasilevskaya <ivasilevskaya@mirantis.com>
2016-07-24 01:49:53 +0300

    lab_resources: fuel-slaves are ignored

--- lab_resources_usage.sh
+++ lab_resources_usage.sh
@@ -12,10 +12,25 @@ SERVERS=(neutron-dev-1 neutron-dev-2 neutron-dev-3 neutron-dev-4)
 # outputs data in csv format (server,env,vlans)
 for s in "${SERVERS[@]}"; do
     for env in $(ssh "$USER@$s" "virsh list | grep $ENVNAME | awk '{print \$2}'"); do
+        # filter out fuel-slaves
+        if [[ "$env" =~ ^fuel-slave-* ]]; then
+            continue;
+        fi
         vlans=$(ssh "$USER@$s" "virsh dumpxml $env | xmllint --xpath \"/domain/devices/interface/vlan/tag/@id\" - 2>/dev/null | awk '{gsub(\"id=\",\"\",\$0); print \$0}' | xargs");
-        mac=$(ssh "$USER@$s" "virsh dumpxml $env | xmllint --xpath \"//devices/interface/mac/@address\" - 2>/dev/null | awk '{gsub(\"address=\",\"\",\$0); print \$0}' | xargs");
-        ip=$(ssh "$USER@$s" "arp -an | grep $mac | grep -Po '\d+\.\d+\.\d+\.\d+'");
-        echo "$s (mac=$mac, ip=$ip, envname=$env, vlans=$vlans)";
+        macs=$(ssh "$USER@$s" "virsh dumpxml $env | xmllint --xpath \"//devices/interface/mac/@address\" - 2>/dev/null | awk '{gsub(\"address=\",\"\",\$0); print \$0}' | xargs");
+        for mac in $macs; do
+            # find first not none ip in broadcast messages
+            mac_ok=$mac
+            ip_ok=$(ssh "$USER@$s" "arp -an | grep $mac | grep -Po '\d+\.\d+\.\d+\.\d+'");
+            if [[ $ip_ok ]]; then
+                break
+            fi
+        done;
+        if [[ $vlans ]]; then
+            echo "$s (mac=$mac_ok, ip=$ip_ok, envname=$env, vlans=$vlans)";
+        else
+            echo "$s (mac=$mac_ok, ip=$ip_ok, envname=$env)";
+        fi
     done;
 done;
 
