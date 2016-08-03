55189707e7cd47b940911852025787f179eef8fa
Inessa Vasilevskaya <ivasilevskaya@mirantis.com>
2016-08-04 00:23:35 +0300

    lab: no more FF cache hacking, override config for deploy

--- lab/send.py
+++ lab/send.py
@@ -10,42 +10,22 @@ import time
 CI_URL = "networking-ci.vm.mirantis.net"
 DEPLOY_URL = "http://%(ci)s:8080/job/deploy_%(type)s_%(server)s/build"
 MANAGE_URL = "http://%(ci)s:8080/job/manage-%(type)s_%(server)s/build"
-COOKIES_PATH = ("/home/ina/.mozilla/firefox/b6ocijug.default/"
-                "sessionstore-backups/recovery.js")
+# TODO(iva) make it configurable?
 PUBLIC_KEY_PATH = "%(home)s/.ssh/id_rsa.pub" % {"home": os.environ["HOME"]}
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
-def _get_auth_cookies():
-    """
-    This implementation fetches cookies from Mozilla's recovery.js file
-    and requires at least one window to CI_URL with user authorized to be open.
-    """
-    with open(COOKIES_PATH) as f:
-        data = f.read()
-    replace_map = {'true': "True",
-                   'false': "False",
-                   'null': "None"}
-    for k, v in replace_map.iteritems():
-        data = data.replace(k, v)
-    data_dicts = ast.literal_eval(data)
-    cookies_list = []
-    for w in data_dicts["windows"]:
-        cookies_list.extend([e for e in [c for c in w["cookies"]]
-                             if e["host"] == CI_URL])
-    return {c["name"]: c["value"] for c in cookies_list}
-
-
-def envnameString(s):
+def _envnameString(s):
     if not s.startswith(('dev_1:', 'dev_2:', 'dev_3:', 'dev_4:')):
         raise argparse.ArgumentTypeError("Environment name should be in format"
                                          " name:server")
     return s
 
 
-def main():
+def parse_args():
+    """Parses arguments by argparse. Returns a tuple (known, unknown"""
     parser = argparse.ArgumentParser()
     parser.add_argument("command", choices=["backup-cluster", "backup-vm",
                                             "revert-cluster", "revert-vm",
@@ -53,17 +33,25 @@ def main():
                                             "deploy-vm"],
                         help="Action to perform")
     parser.add_argument("env", help="Environment name in format 'server:name'",
-                        type=envnameString)
+                        type=_envnameString)
     parser.add_argument("--snapshot", help="Snapshot name")
-    parser.add_argument("--user", help="Jenkins API user")
-    parser.add_argument("--token", help="Jenkins API token")
+    parser.add_argument("--user", help="Jenkins API user", required=True)
+    parser.add_argument("--token", help="Jenkins API token", required=True)
     parser.add_argument("--config", help="Configuration file for deploy job")
-    parsed = parser.parse_args()
-    auth_data = None
+    return parser.parse_known_args()
+
+
+def main():
+    parsed, unknown = parse_args()
     vm_type = parsed.command.split('-')[1]
     server, env = parsed.env.split(':')
-    if parsed.user and parsed.token:
-        auth_data = {"user": parsed.user, "token": parsed.token}
+    # turn unknown args into optional arguments
+    # args should be passed as --OVERRIDE_ARGUMENT=VALUE
+    override = dict(tuple(arg.split('=', 1))
+                    for arg in [u.replace('--', '') for u in unknown])
+    override["DOMAIN"] = env
+    auth_data = {"user": parsed.user, "token": parsed.token}
+    # TODO(iva) make other jobs utilize config/override args?
     if parsed.command.startswith("backup"):
         backup(server=server, env=env, bkp=parsed.snapshot,
                vm_type=vm_type, auth_data=auth_data)
@@ -78,8 +66,8 @@ def main():
         if not parsed.config:
             LOG.error("'Config' parameter must be set for deploy-* job")
             return
-        deploy(server=server, env=env, vm_type=vm_type, config=parsed.config,
-               auth_data=auth_data)
+        deploy(server=server, vm_type=vm_type, config=parsed.config,
+               auth_data=auth_data, override=override)
     else:
         raise NotImplemented("Command %s not implemented yet" % parsed.command)
 
@@ -90,7 +78,6 @@ def revert(server, env, bkp, vm_type, auth_data):
         return
     data = {"DOMAIN": env,
             "SNAP_NAME": bkp,
-            # TODO(iva) optional storage pool?
             "STORAGE_POOL": "big",
             "OPERATION": "revert-%s" % vm_type}
     url = MANAGE_URL % {"server": server, "ci": CI_URL, "type": vm_type}
@@ -102,7 +89,6 @@ def backup(server, env, bkp, vm_type, auth_data):
         bkp = "bkp_%s" % time.time()
     data = {"DOMAIN": env,
             "SNAP_NAME": bkp,
-            # TODO(iva) optional storage pool?
             "STORAGE_POOL": "big",
             "OPERATION": "snapshot-%s" % vm_type}
     url = MANAGE_URL % {"server": server, "ci": CI_URL, "type": vm_type}
@@ -117,23 +103,32 @@ def cleanup(server, env, vm_type, auth_data):
 
 
 # TODO(iva) cluster deploy with configurations?
-def deploy(server, env, vm_type, config, auth_data):
+def deploy(server, vm_type, config, auth_data, override):
     if vm_type != "vm":
         raise NotImplemented("Cluster deployments not supported yet")
-    data = _data_from_config(config, env)
+    data = _data_from_config(config, override=override)
     url = DEPLOY_URL % {"server": server, "ci": CI_URL, "type": vm_type}
     _send_request(url, data, auth_data)
 
 
-def _data_from_config(config, env):
+def _data_from_config(config, override=None):
+    """Transfer config values in data to be submitted.
+
+       If override dictionary is passed, then the values from it override those
+       in config.
+    """
     cfg = ConfigParser.ConfigParser()
     cfg.read(config)
     data = {k.upper(): v for k, v in cfg.items('default')}
-    # TODO(iva) allow unknows argparse parameters to override those set in
-    # config?
-    with open(PUBLIC_KEY_PATH) as f:
-        data["PUBLIC_KEY"] = f.read()
-    data["DOMAIN"] = env
+    # TODO(iva) make public_key a usual parameter and pass with override?
+    if "PUBLIC_KEY" in data:
+        with open(PUBLIC_KEY_PATH) as f:
+            data["PUBLIC_KEY"] = f.read()
+    if not override:
+        return data
+    for param in {k: v for k, v in override.iteritems() if k in data}:
+        data[param] = override[param]
+    # TODO(iva) any validation?
     return data
 
 
@@ -141,15 +136,9 @@ def _send_request(url, data, auth_data):
     params = {"delay": "0sec"}
     data["json"] = json.dumps({"parameter": [{"name": k, "value": v}
                                              for k, v in data.iteritems()]})
-    # if user provides a token -> use it, otherwise try to find some cookies in
-    # FF sessions backup file
-    if not auth_data:
-        r = requests.post(url, data=data, params=params,
-                          cookies=_get_auth_cookies())
-    else:
-        r = requests.post(url, data=data, params=params,
-                          auth=requests.auth.HTTPBasicAuth(auth_data['user'],
-                                                           auth_data['token']))
+    r = requests.post(url, data=data, params=params,
+                      auth=requests.auth.HTTPBasicAuth(auth_data['user'],
+                                                       auth_data['token']))
     if r.status_code == 201:
         LOG.info("Success!")
     else:
