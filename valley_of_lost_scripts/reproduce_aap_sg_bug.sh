be7e5b7f509a6db85eb5ec3964f006adb1161453
Inessa Vasilevskaya <ivasilevskaya@mirantis.com>
2017-03-01 18:19:14 +0000

    vols: pinger and aap_repr minor refactor

--- valley_of_lost_scripts/reproduce_aap_sg_bug.sh
+++ valley_of_lost_scripts/reproduce_aap_sg_bug.sh
@@ -3,8 +3,9 @@ FAKE_SG_ID="fake_sg_id"
 NETWORK_NAME="admin_internal_net"
 GATEWAY="192.168.0.1"
 CIDR="192.168.0.0/24"
-IMG="cirros-0.3.4-x86_64-uec"
+IMG="cirros-0.3.5-x86_64-disk"
 VM="vm"
+CIDR="192.168.0.0/28"
 
 function init {
     neutron security-group-create $ALL_ALLOWED_SG_ID;
@@ -13,7 +14,7 @@ function init {
     neutron security-group-rule-create $FAKE_SG_ID --direction ingress --protocol tcp --port-range-max 22 --port-range-min 22;
     neutron net-create $NETWORK_NAME
     neutron subnet-create "$NETWORK_NAME" --name "$NETWORK_NAME-subnet" --gateway $GATEWAY $CIDR
-#    neutron port-create $NETWORK_NAME --security-group=$ALL_ALLOWED_SG_ID
+    neutron port-create $NETWORK_NAME --security-group=$ALL_ALLOWED_SG_ID
 }
 
 function boot_vms {
@@ -28,7 +29,7 @@ function update_with_huge_cidr {
     all_allowed_address=$(nova list | grep "$VM-all-allowed" | awk '{gsub(".*=", "", $12); print $12}' );
     echo $all_allowed_address;
     all_allowed_port=$(neutron port-list | grep "$all_allowed_address" | awk '{print $2}');
-    neutron port-update $all_allowed_port --allowed-address-pairs type=dict list=true ip_address=128.0.0.0/1
+    neutron port-update $all_allowed_port --allowed-address-pairs type=dict list=true ip_address=$CIDR
     echo "Allowing traffic from FAKE_SG_ID vms to ALL_ALLOWED_VMS"
     neutron security-group-rule-create $FAKE_SG_ID --remote-group-id $ALL_ALLOWED_SG_ID --direction ingress
 }
--- valley_of_lost_scripts/verify_ping.sh
+++ valley_of_lost_scripts/verify_ping.sh
@@ -4,17 +4,22 @@ NETNAME="admin_internal_net"
 QDHCP="qdhcp-$(neutron net-list | grep $NETNAME | awk '{print $2}')"
 OUTPUT="badips"
 PING_ARGS="-c 1"
+IMAGE_PASS="cubswin:)"
 
 echo "" > $OUTPUT
 
-for ip in $(nova list | grep 'ACTIVE' | awk '{print $12}' | sed "s/$NETNAME=//g"); do
-    sudo ip netns e $QDHCP ping "$PING_ARGS" $ip
-    if [ $? -eq 0 ]; then
-        continue
-    else
-        echo "$ip can't be reached"
-        echo "$ip" >> $OUTPUT
-    fi
+all_ips=$(nova list | grep 'ACTIVE' | awk '{print $12}' | sed "s/$NETNAME=//g")
+
+for ip in $all_ips; do
+    for other_ip in $all_ips; do
+        echo "ping $PING_ARGS $other_ip" | sudo ip netns e $QDHCP sshpass -p "$IMAGE_PASS" ssh cirros@$ip;
+        if [ $? -eq 0 ]; then
+            continue
+        else
+            echo "$other_ip can't be reached from $ip"
+            echo "$ip->$other_ip" >> $OUTPUT
+        fi
+    done
 done
 
 echo "COULD NOT REACH: $(cat $OUTPUT)"
