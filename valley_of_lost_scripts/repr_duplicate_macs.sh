43b3edeba96c38a2e226e3398515a36ebfa138e9
Inessa Vasilevskaya <ivasilevskaya@mirantis.com>
2017-06-19 17:56:47 +0000

    vols: non unique macs testcases made a bit cleaner

--- valley_of_lost_scripts/repr_duplicate_macs.sh
+++ valley_of_lost_scripts/repr_duplicate_macs.sh
@@ -1,48 +1,79 @@
+NET_PREFIX_DHCP="mac_net_dhcp"
+NET_PREFIX_PHYS="mac_net_phys"
+START_PORT_NUM=100500
+SG="mac_sg"
+
 function setup {
-    openstack network create net0
-    openstack network create net1
-    openstack network create net2
-    openstack subnet create --network net0 --subnet-range 10.0.4.0/24 subnet0 
-    openstack subnet create --network net1 --subnet-range 10.0.5.0/24 subnet1
-    openstack subnet create --network net2 --subnet-range 10.0.6.0/24 subnet2
+    for num in {0,1,2}; do
+        net="$NET_PREFIX_DHCP$num"
+        subnet="subnet_$net"
+        cidr="10.0.$((42 + $num)).0/24"
+        openstack network create $net;
+        openstack subnet create --network $net --subnet-range $cidr $subnet
+    done
 }
 
 function create_vms_ports {
-    openstack port create --network net0 port0
-    mac="$( openstack port show port0 | awk '/ mac_address / { print $4 }' )"
-    openstack port create --network net1 --mac-address "$mac" port1
-    openstack port create --network net2 --mac-address "$mac" port2
+    net_num=0
+    net="$NET_PREFIX_DHCP$net_num"
+    port="port$START_PORT_NUM"
+    openstack port create --network $net $port
+    mac="$( openstack port show $port | awk '/ mac_address / { print $4 }' )"
+    for num in {1,2}; do
+        net="$NET_PREFIX_DHCP$num"
+        port_number=$(($num + $START_PORT_NUM))
+        port="port$port_number"
+        openstack port create --network $net --mac-address "$mac" $port
+    done
 }
 
 function create_vms {
     cirros_img="$(glance image-list | grep cirros | awk '{print $2}')"
-    openstack server create --flavor 1 --image $cirros_img --nic port-id=port0  --wait vm0
-    openstack server create --flavor 1 --image $cirros_img --nic port-id=port1  --wait vm1
-    openstack server create --flavor 1 --image $cirros_img --nic port-id=port2  --wait vm2
+    for num in {0,1,2}; do
+        port="port$(( $START_PORT_NUM + $num))"
+        vm="vm$port"
+        openstack server create --flavor 1 --image $cirros_img --nic port-id=$port  --wait $vm
+    done
     openstack server list
 }
 
 function assign_sg {
-    neutron security-group-create SG;
-    neutron security-group-rule-create SG;
-    sg="SG"
-    for port in port{0,1,2}; do
+    neutron security-group-create $SG;
+    neutron security-group-rule-create $SG;
+    for num in {0,1,2}; do
+        port="port$(($START_PORT_NUM + $num))"
         port_id=$(neutron port-list | grep $port | awk '{print $2}')
-        echo "$port_id"
-        neutron port-update $port_id --security-group $sg;
+        neutron port-update $port_id --security-group $SG;
     done
 }
 
-function ping {
-    vm0_ip="$(nova list | grep vm0 | awk '{print $12}' | sed -e 's/.*=//g')"
-    vm1_ip="$(nova list | grep vm1 | awk '{print $12}' | sed -e 's/.*=//g')"
-    vm2_ip="$(nova list | grep vm2 | awk '{print $12}' | sed -e 's/.*=//g')"
-    # packets lost
-    sudo ip netns exec "qdhcp-$( openstack network show net0 | awk '/ id / { print $4 }' )" ping -c 3 $vm0_ip
-    # packets lost
-    sudo ip netns exec "qdhcp-$( openstack network show net1 | awk '/ id / { print $4 }' )" ping -c 3 $vm1_ip
-    # works
-    sudo ip netns exec "qdhcp-$( openstack network show net2 | awk '/ id / { print $4 }' )" ping -c 3 $vm2_ip
+function ping_dhcp {
+    for num in {0,1,2}; do
+        port="port$(( $START_PORT_NUM + $num))"
+        vm="vm$port"
+        vm_ip="$(nova list | grep $vm | awk '{print $12}' | sed -e 's/.*=//g')"
+        net="$NET_PREFIX_DHCP$num"
+        sudo ip netns exec "qdhcp-$( openstack network show $net | awk '/ id / { print $4 }' )" ping -c 3 $vm_ip
+    done
+}
+
+function cleanup_dhcp {
+    for num in {0,1,2}; do
+        # delete vm
+        port="port$(( $START_PORT_NUM + $num))"
+        vm="vm$port"
+        net="$NET_PREFIX_DHCP$num"
+        subnet=$(neutron net-show $net -c subnets | awk '{print $4}' | tail -n2 | xargs)
+        nova delete $vm
+        # delete port
+        neutron port-delete $port
+        # delete subnet
+        neutron subnet-delete $subnet
+        # delete network
+        neutron net-delete $net
+    done
+    # delete security-group
+    neutron security-group-delete $SG
 }
 
 function create_phys_bridge {
@@ -50,31 +81,77 @@ function create_phys_bridge {
     sudo ovs-vsctl add-port br-test port0 -- set interface port0 type=internal
     sudo ip link add link port0 name port100 type vlan id 100
     sudo ip link add link port0 name port101 type vlan id 101
-    sudo ip address add 10.0.4.3/24 dev port100
-    sudo ip address add 10.0.5.3/24 dev port101
+    sudo ip address add 42.42.0.3/24 dev port100
+    sudo ip address add 42.42.1.3/24 dev port101
     sudo ip link set up dev port0
     sudo ip link set up dev port100
     sudo ip link set up dev port101
 }
 
 function create_phys_nets_vms {
-    openstack network create net0 --provider-network-type vlan --provider-physical-network test --provider-segment 100
-    openstack network create net1 --provider-network-type vlan --provider-physical-network test --provider-segment 101
-    openstack subnet create subnet0 --network net0 --subnet-range 10.0.4.0/24
-    openstack subnet create subnet1 --network net1 --subnet-range 10.0.5.0/24
-    neutron security-group-create SG;
-    neutron security-group-rule-create SG;
-    openstack port create port0 --network net0 --mac-address fa:16:3e:d7:56:3d --security-group SG
-    openstack port create port1 --network net1 --mac-address fa:16:3e:d7:56:3d --security-group SG
-    openstack server create vm0 --flavor 1 --image cirros-0.3.5-x86_64-disk --nic port-id=port0 --wait
-    openstack server create vm1 --flavor 1 --image cirros-0.3.5-x86_64-disk --nic port-id=port1 --wait
+    for num in {0,1}; do
+        net="$NET_PREFIX_PHYS$num"
+        subnet="subnet$num"
+        cidr="42.42.$(($num)).0/24"
+        openstack network create $net --provider-network-type vlan --provider-physical-network test --provider-segment 100
+        openstack subnet create $subnet --network $net --subnet-range $cidr
+    done
+    exists=$(neutron security-group-list | grep $SG)
+    if ![[ $exists ]]; then
+        echo "No security froup $SG found, will create one"
+        neutron security-group-create $SG
+        # allow all
+        neutron security-group-rule-create $SG
+    fi
+    for num in {0,1}; do
+        net="$NET_PREFIX_PHYS$num"
+        port="port_phys$num"
+        vm="vm$num"
+        mac=fa:16:3e:d7:56:3d 
+        openstack port create $port --network $net --mac-address fa:16:3e:d7:56:3d --security-group $SG
+        openstack server create $vm --flavor 1 --image cirros-0.3.5-x86_64-disk --nic port-id=$port --wait
+    done
 }
 
-#setup
-#create_vms_ports
-#create_vms
-#assign_sg
-#ping
+function cleanup_phys_br {
+    for num in {0,1}; do
+        vm="vm$num"
+        port="port_phys$num"
+        net="$NET_PREFIX_PHYS$num"
+        # delete vm
+        nova delete $vm
+        # delete port
+        neutron port-delete $port
+        # delete network
+        neutron net-delete $net
+    done
+    sudo ip link delete port0 
+    sudo ovs-vsctl del-br br-test
+}
 
-create_phys_bridge
-create_phys_nets_vms
+function ping_phys_br {
+    for num in {0,1}; do
+        vm="vm$num"
+        vm_ip="$(nova list | grep $vm | awk '{print $12}' | sed -e 's/.*=//g')"
+        ping -c 3 $vm_ip
+    done
+}
+
+function testcase_dhcp {
+    cleanup_dhcp
+    setup
+    create_vms_ports
+    assign_sg
+    create_vms
+    ping_dhcp
+}
+
+function testcase_vlan_phys_br {
+    cleanup_phys_br
+    create_phys_bridge
+    create_phys_nets_vms
+    # ping_phys_br
+}
+
+# testcase_dhcp
+testcase_vlan_phys_br
